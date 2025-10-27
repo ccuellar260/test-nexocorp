41485cc9ce1d9ce98a6a59ac49a83a32c743755e
Cristian Cuellar <ccuellar260@gmail.com>
2025-10-27 09:28:46 -0400

    Estructura inicial del proyecto

---
+++ Farmacorp.PosExpress.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Farmacorp.PosExpress.Domain.Entities;
+
+
+namespace Farmacorp.PosExpress.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Farmacorp.PosExpress.Presentation.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Farmacorp.PosExpress.Infrastructure.Data;
+
+Console.WriteLine("HOLA MUNDO !!!!!!!");
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    Console.WriteLine("Base de datos conectada correctamente.");
+}
