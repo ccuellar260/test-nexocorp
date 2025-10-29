f7ba5da6390dbda7345946240b04d5cf2e9163f7
Cristian Cuellar <ccuellar260@gmail.com>
2025-10-29 14:13:03 -0400

    cambio de nombre de la capa de presentacion
    - antes: Farmacorp.PosExpress.Presentation.Console
    - ahora: Farmacorp.PosExpress.Presentation

---
+++ Farmacorp.PosExpress.Presentation/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Farmacorp.PosExpress.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("HOLA MUNDO !!!!!!!");
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+Console.WriteLine($"connectionString: {connectionString}");
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    try
+    {
+        // conectar a la base de datos
+        await db.Database.CanConnectAsync();
+        Console.WriteLine("Base de datos conectada correctamente.");
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error al conectar la db: {ex.Message}");
+        Console.WriteLine($"StackTrace: {ex.StackTrace}");
+    }
+}
