0480d4e10f13b9d3420fe6de66e60b68fb4de8d2
Cristian Cuellar <ccuellar260@gmail.com>
2025-10-29 14:17:52 -0400

    correccion en la configuracion para reconocer a appsetting.json

--- Farmacorp.PosExpress.Presentation/Program.cs
+++ Farmacorp.PosExpress.Presentation/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 using Farmacorp.PosExpress.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +9,15 @@ using Microsoft.Extensions.Hosting;
 Console.WriteLine("HOLA MUNDO !!!!!!!");
 
 var builder = Host.CreateApplicationBuilder(args);
+
+//por si no reconoce el appsettings.json, agregar manualmente la ruta base
+builder.Environment.ContentRootPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? Directory.GetCurrentDirectory();
+builder.Configuration
+   .SetBasePath(builder.Environment.ContentRootPath)
+   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+   .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+//end de agregar manualmente la ruta base
+
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -19,7 +29,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 );
 
 
+
 var app = builder.Build();
+
 
 using (var scope = app.Services.CreateScope())
 {
@@ -27,14 +39,15 @@ using (var scope = app.Services.CreateScope())
 
     try
     {
-        // conectar a la base de datos
+        // Intenta conectar a la base de datos
         await db.Database.CanConnectAsync();
         Console.WriteLine("Base de datos conectada correctamente.");
 
+      
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"Error al conectar la db: {ex.Message}");
+         Console.WriteLine($"Error al conectar la db: {ex.Message}");
         Console.WriteLine($"StackTrace: {ex.StackTrace}");
     }
 }
